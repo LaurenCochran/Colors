4b2ad4ca9466b385860e3c9cb490eacd9c3bb4e7
Lauren <Owner1@MacBooks-MacBook-Pro.local>
2019-03-08 19:22:47 -0600

    added requirements

--- Colors/ColorsViewController.swift
+++ Colors/ColorsViewController.swift
@@ -8,23 +8,32 @@
 
 import UIKit
 
-class ColorsViewController: UIViewController {
+class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        let abbr = colors[indexPath.row]
+        cell.textLabel?.text = colors[indexPath.row]
+        cell.backgroundColor = UIColor.init(named: abbr)
+        return cell
+    }
+    
+
+    
 
 }
